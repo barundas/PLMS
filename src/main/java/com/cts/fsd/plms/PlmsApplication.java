d28653c114619d7d32d2caace7d1570c2248ac8d
Barun <barun.das@gmail.com>
2017-11-21 23:07:43 +0530

    Initial commit

---
+++ src/main/java/com/cts/fsd/plms/PlmsApplication.java
@@ -0,0 +1,12 @@
+package com.cts.fsd.plms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlmsApplication.class, args);
+	}
+}
---
+++ src/main/java/com/cts/fsd/plms/configuration/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.cts.fsd.plms.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.requestCache().requestCache(new NullRequestCache());
+		http.httpBasic();
+		http.authorizeRequests().antMatchers("/").permitAll().anyRequest().fullyAuthenticated()
+		.and()
+		.exceptionHandling().accessDeniedHandler(null);
+	}
+}
